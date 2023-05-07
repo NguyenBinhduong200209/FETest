da00af4ecf2d66474066bd2f4b62642bf5288df2
Duong_va_nhung_nguoi_ban <nguyenbinhduong200209@gmail.com>
2023-05-07 22:00:10 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import TodoList from "./TodoList";
+import TodoListHeader from "./TodoListHeader";
+import Form from "./Form";
+import Footer from "./Footer";
+import { Routes, Route } from "react-router-dom";
+import { useState,createContext } from "react";
+export const NewTask = createContext();
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
 
-export default App;
+const Home = () => {
+  const [show, setShow] = useState(true);
+
+  const [task, setTask] = useState([
+    { id: 1, name: "Build some websites", check: true },
+    { id: 2, name: "Do excercises", check: false }
+  ]);
+
+  if (!JSON.parse(localStorage.getItem("listTask"))) {
+    localStorage.setItem("listTask", JSON.stringify([...task]));
+  }
+
+  return (
+    <NewTask.Provider
+      value={{ task: task, setTask: setTask, show: show, setShow: setShow }}
+    >
+      <div className="App">
+        <div className="container">
+          <TodoListHeader />
+          <TodoList />
+          <Form />
+        </div>
+        <Footer />
+      </div>
+    </NewTask.Provider>
+  );
+};
