25e05382206cdc91bf671203b94519b8ad791c36
Duong_va_nhung_nguoi_ban <nguyenbinhduong200209@gmail.com>
2023-05-08 01:48:05 +0700

    hoanthanh

--- src/Form.js
+++ src/Form.js
@@ -1,27 +1,32 @@
-import { useState, useContext } from "react";
+import { useContext, useState } from "react";
 import { NewTask } from "./App";
 
 const Form = () => {
+  const [inputnewtasktest, setInputNewTask] = useState("");
   const ctxTask = useContext(NewTask);
-  let inputnewtask;
   const inputTask = (e) => {
-    inputnewtask = e.target.value;
+    setInputNewTask(e.target.value);
   };
   const handleInput = (e) => {
     e.preventDefault();
     const newListTask = ctxTask.task;
     newListTask.push({
       id: Math.floor(Math.random() * 40),
-      name: inputnewtask,
-      check: true
+      name: inputnewtasktest,
+      check: false
     });
     ctxTask.setTask([...newListTask]);
 
     localStorage.setItem("listTask", JSON.stringify([...ctxTask.task]));
+    setInputNewTask("");
   };
   return (
     <form className="form">
-      <input placeholder="Enter task ..." onChange={inputTask} />
+      <input
+        placeholder="Enter task ..."
+        onChange={inputTask}
+        value={inputnewtasktest}
+      />
       <button onClick={handleInput}>Submit</button>
     </form>
   );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
-import "./i18n/i18n"
+
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
